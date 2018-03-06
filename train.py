9d4f1cf6e8bf9bac2037f1400ed2c94d5f1a7e8d
vic006 <vedavikas06@gmail.com>
2018-03-07 02:39:03 +0530

    Obtained an accuracy of 0.863636363636(bench mark)

--- train.py
+++ train.py
@@ -1,37 +1,41 @@
+import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D
-from keras.layers import Flatten, Dense
-
-img_width, img_height = 100, 100
+from keras.layers import Flatten, Dense,Dropout
+import matplotlib.pyplot as plt
 
 train_data_dir = 'data3D/training'
 
 validation_data_dir = 'data3D/validation'
 
-train_samples = 295
+train_samples = 301
 
-validation_samples = 150
+validation_samples = 151
 
-epoch = 20
+epoch = 10
+
+img_width, img_height = 100, 100
+
 
 # ** Model Begins **
 model = Sequential()
 model.add(Convolution2D(20, 3, 3, activation='relu', input_shape=(img_width, img_height,3)))
 model.add(MaxPooling2D(2, 2))
 
-model.add(Convolution2D(20, 5, 5, activation='relu'))
+model.add(Convolution2D(30, 5, 5, activation='relu'))
 model.add(MaxPooling2D(2, 2))
 
 model.add(Flatten())
 model.add(Dense(1000, activation='relu'))
+model.add(Dropout(0.5))
 
 model.add(Dense(3, activation='softmax'))
 model.summary()
 # ** Model Ends **
 
 model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
+              optimizer=keras.optimizers.Adam(),
               metrics=['accuracy'])
 
 train_datagen = ImageDataGenerator(
@@ -53,7 +57,7 @@ validation_generator = test_datagen.flow_from_directory(
         batch_size=10,
         class_mode='categorical')
 
-model.fit_generator(
+history = model.fit_generator(
         train_generator,
         samples_per_epoch=train_samples,
         nb_epoch=epoch,
@@ -68,11 +72,11 @@ generator = test_datagen.flow_from_directory(
         class_mode=None,  # only data, no labels
         shuffle=False)  # keep data in same order as labels
 
-probabilities = model.predict_generator(generator, 45)
+probabilities = model.predict_generator(generator, 66)
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 import numpy as np
-y_true = np.array([0] * 20 + [1] * 25)
+y_true = np.array([0] * 20 + [1] * 25 + [2] * 21)
 #y_pred = probabilities > 0.5
 print(probabilities)
 y_pred = np.argmax(probabilities,axis=1)
@@ -81,6 +85,25 @@ y_pred = np.argmax(probabilities,axis=1)
 print(confusion_matrix(y_true, y_pred))
 
 print(accuracy_score(y_true, y_pred))
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
 
 
 
